73917a2b5ded26ef3d906db23bf482da585cb462
G3FRY <gefry666@gmail.com>
2021-09-08 09:11:08 -0500

    Cambiando string de conexión y agregando modelo para la tabla de director

---
+++ src/model/DirectorModel.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author gefry
+ */
+public class DirectorModel {
+    private int director_id;
+    private String nombre;
+    private String apellido;
+    private String nacionalidad;
+    
+    public DirectorModel(int director_id_,String nombre_,String apellido_,String nacionalidad_)
+    {
+        this.director_id = director_id_;
+        this.nombre = nombre_;
+        this.apellido = apellido_;
+        this.nacionalidad = nacionalidad_;
+    }
+    
+    public int getdirector_id()
+    {
+            return this.director_id;
+    }
+
+    public String getnombre()
+    {
+            return this.nombre;
+    }
+    
+    public void setnombre(String value)
+    {
+            this.nombre = value;
+    }
+    
+    public String getapellido()
+    {
+            return this.apellido;
+    }
+    public void setapellido(String value)
+    {
+            this.apellido = value;
+    }
+
+    public String getnacionalidad()
+    {
+            return this.nacionalidad;
+    }
+    public void setnacionalidad(String value)
+    {
+            this.nacionalidad = value;
+    }
+    
+}
--- src/utils/ConnectionDB.java
+++ src/utils/ConnectionDB.java
@@ -42,11 +42,10 @@ public class ConnectionDB {
             String port     = (String)jsonObject.get("dp_port");
             String username = (String)jsonObject.get("db_user");
             String password = (String)jsonObject.get("db_pssword");
-            String dbURL = "jdbc:mysql://"+host+":"+port+"/museo" ;
-            
+            String dbURL = "jdbc:mysql://"+host+":"+port+"/reto_4" ;
             conn = DriverManager.getConnection(dbURL, username, password);
-            //if( conn != null ) 
-            //    System.out.println ( "Conectado" );
+            if( conn != null ) 
+                System.out.println ("Conectado");
         } 
         catch( SQLException | FileNotFoundException ex ) {
             ex.printStackTrace();
@@ -57,7 +56,7 @@ public class ConnectionDB {
         
         return conn;
     }
-    
+      
     public static void main(String[] args){
         getConnection();
     }
